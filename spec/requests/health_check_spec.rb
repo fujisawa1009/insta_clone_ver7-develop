ab368dea88dd23b0f530287ad7ca2a3925e355fe
yuta.fujisawa <mt.fuji1009@gmail.com>
2023-07-28 00:50:25 +0900

    add rspec

---
+++ spec/requests/health_check_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe('HealthChecks', type: :request) do
+  describe 'ヘルスチェック' do
+    it '200のHTTPステータスが返る' do
+      get '/health_check'
+      expect(response).to have_http_status :ok
+    end
+  end
+end
