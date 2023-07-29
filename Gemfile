02750546175c6c6ea9e0090cc002700a2cc4a96f
yuta.fujisawa <mt.fuji1009@gmail.com>
2023-07-29 15:14:46 +0900

    add i18n エラーの日本語化

--- Gemfile
+++ Gemfile
@@ -49,6 +49,8 @@ gem 'bootsnap', require: false
 # gem "image_processing", "~> 1.2"
 # 認証
 gem 'sorcery'
+# 国際化
+gem 'rails-i18n'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,13 @@ module InstaCloneVer7
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.helper false
+    end
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
   end
 end
