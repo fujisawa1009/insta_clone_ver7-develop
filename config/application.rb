c886eaffc81bed983be35a6f2b305b17981db1a1
yuta.fujisawa <mt.fuji1009@gmail.com>
2023-07-29 15:42:04 +0900

    fix バリデーションエラー時にビューが崩れる問題

--- config/application.rb
+++ config/application.rb
@@ -40,6 +40,9 @@ module InstaCloneVer7
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+
     config.i18n.default_locale = :ja
   end
 end
