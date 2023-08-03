5ba412b98bc5831014454b856779976413eb2781
yuta.fujisawa <mt.fuji1009@gmail.com>
2023-08-03 18:07:44 +0900

    add# postモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_25_001244) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_03_090247) do
+  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
+    t.text "body", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
@@ -22,4 +30,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_25_001244) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
