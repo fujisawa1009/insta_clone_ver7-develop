523eea3efa8cfc8bfcf522c8a75c874e3db9cd46
yuta.fujisawa <48338865+fujisawa1009@users.noreply.github.com>
2023-08-06 15:13:09 +0900

    add ポストのページネーション実装 (#4)

---
+++ config/initializers/pagy.rb
@@ -0,0 +1,4 @@
+# NOTE: https://github.com/ddnexus/pagy/blob/master/lib/config/pagy.rb
+Pagy::DEFAULT[:items] = 15
+require 'pagy/extras/bootstrap'
+require 'pagy/extras/i18n'
--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe '投稿', type: :system do
-  pending("調査中")
   let!(:user) { create(:user) }
   before do
     login_as(user)
   end
   describe '投稿' do
     it '投稿ができること' do
-      pending("調査中")
       within '#header' do
         click_on '投稿'
       end
@@ -22,7 +20,6 @@ RSpec.describe '投稿', type: :system do
   describe '編集' do
     let!(:post) { create(:post, user: user) }
     it '編集ができること' do
-      pending("調査中")
       visit "/posts/#{post.id}"
       find('#postDropdownMenuLink').click
       click_on '編集'
@@ -34,15 +31,25 @@ RSpec.describe '投稿', type: :system do
   end
 
   describe '削除' do
-
     let!(:post) { create(:post, user: user) }
     it '削除ができること' do
-      pending("調査中")
       visit "/posts/#{post.id}"
       find('#postDropdownMenuLink').click
       accept_confirm { click_on '削除' }
       expect(page).to have_content '投稿を削除しました'
       expect(page).not_to have_content post.body
+    end
+  end
+
+  describe 'ページネーション' do
+    let!(:post) { create(:post, created_at: Time.current.yesterday) }
+    before do
+      create_list(:post, 15)
+    end
+
+    it '16件目のポストは1ページ目に表示されていないこと' do
+      visit '/posts'
+      expect(page).not_to have_css("#post_#{post.id}")
     end
   end
 end
