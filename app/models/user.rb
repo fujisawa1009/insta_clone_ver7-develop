b32e5ddfa583f67316ac9fc3d1bb4bb94ee7b7fb
yuta.fujisawa <mt.fuji1009@gmail.com>
2023-08-09 19:45:12 +0900

    fix# rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,9 +23,11 @@ class User < ApplicationRecord
   has_many :like_posts, through: :likes, source: :post
 
   has_many :active_relationships, class_name: 'Relationship',
+                                  inverse_of: :follower,
                                   foreign_key: 'follower_id',
                                   dependent: :destroy
   has_many :passive_relationships, class_name: 'Relationship',
+                                   inverse_of: :followed,
                                    foreign_key: 'followed_id',
                                    dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
