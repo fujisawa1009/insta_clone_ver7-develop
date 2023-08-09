585771219d85f003c9d2a1acd69b82a94d83f8a0
yuta.fujisawa <mt.fuji1009@gmail.com>
2023-08-09 19:51:52 +0900

    add フォロー, アンフォローの機能を実装した

---
+++ app/controllers/users/relationships_controller.rb
@@ -0,0 +1,13 @@
+class Users::RelationshipsController < ApplicationController
+  before_action :require_login
+
+  def create
+    @user = User.find(params[:user_id])
+    current_user.follow(@user)
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    current_user.unfollow(@user)
+  end
+end
