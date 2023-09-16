e97797c0d749f1b58791ba9b0d23de632f55ce28
yuta.fujisawa <mt.fuji1009@gmail.com>
2023-09-16 17:05:39 +0900

    add 投稿へのいいね時の通知

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -4,10 +4,18 @@ class Posts::LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     current_user.like(@post)
+    create_notifications_about_like(@post)
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     current_user.unlike(@post)
+  end
+
+  private
+
+  def create_notifications_about_like(post)
+    notification = Notification.create!(title: "#{current_user.username}さんがあなたの投稿をいいねしました", url: post_url(post))
+    notification.notify(post.user)
   end
 end
