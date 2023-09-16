edb320e53ec9418eb891983e0dcbf28fc7939dff
yuta.fujisawa <mt.fuji1009@gmail.com>
2023-09-16 17:00:24 +0900

    add フォロー時の通知

--- app/controllers/users/relationships_controller.rb
+++ app/controllers/users/relationships_controller.rb
@@ -4,10 +4,18 @@ class Users::RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     current_user.follow(@user)
+    create_notifications_about_follow(@user)
   end
 
   def destroy
     @user = User.find(params[:user_id])
     current_user.unfollow(@user)
+  end
+
+  private
+
+  def create_notifications_about_follow(user)
+    notification = Notification.create!(title: "#{current_user.username}さんにフォローされました", url: user_url(current_user))
+    notification.notify(user)
   end
 end
