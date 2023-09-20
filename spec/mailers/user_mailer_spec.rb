2eaa7b5389a495f313a733e8adfd19568905b103
yuta.fujisawa <mt.fuji1009@gmail.com>
2023-09-20 23:44:55 +0900

    fix#2 rubocop

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -18,10 +18,10 @@ RSpec.describe UserMailer, type: :mailer do
     let(:user) { create(:user) }
     let(:mail) { UserMailer.with(user_from: user, user_to: comment.post.user, comment: comment).comment_post }
     it '想定通りのメールが作成されている' do
-      expect(mail.subject).to eq "#{user.username}さんがあなたの投稿にコメントしました"
+      expect(mail.subject).to eq "#{user.username}さんがあなたの投稿にいいねしました"
       expect(mail.to).to eq [comment.post.user.email]
       expect(mail.from).to eq ['instaclone@example.com']
-      expect(mail.body.encoded).to match("#{user.username}さんがあなたの投稿にコメントしました")
+      expect(mail.body.encoded).to match("#{user.username}さんがあなたの投稿にいいねしました")
     end
   end
 
