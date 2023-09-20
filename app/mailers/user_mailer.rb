6b85cfb202e78db034c28e370f876c8e55164dc1
yuta.fujisawa <mt.fuji1009@gmail.com>
2023-09-20 23:30:55 +0900

    add メーラーのスペックを書いた

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,19 +4,19 @@ class UserMailer < ApplicationMailer
     @user_from = params[:user_from]
     @user_to = params[:user_to]
     @post = params[:post]
-    mail(to: @user_to.email, subject: "#{@user_from.username}があなたの投稿にいいねしました")
+    mail(to: @user_to.email, subject: "#{@user_from.username}さんがあなたの投稿にいいねしました")
   end
 
   def comment_post
     @user_from = params[:user_from]
     @user_to = params[:user_to]
     @comment = params[:comment]
-    mail(to: @user_to.email, subject: "#{@user_from.username}があなたの投稿にいいねしました")
+    mail(to: @user_to.email, subject: "#{@user_from.username}さんがあなたの投稿にいいねしました")
   end
 
   def follow
     @user_from = params[:user_from]
     @user_to = params[:user_to]
-    mail(to: @user_to.email, subject: "#{@user_from.username}があなたをフォローしました")
+    mail(to: @user_to.email, subject: "#{@user_from.username}さんがあなたをフォローしました")
   end
 end
