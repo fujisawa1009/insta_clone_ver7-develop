22d6d1ccc8889d4acf92acd6210179ac7a660ace
yuta.fujisawa <mt.fuji1009@gmail.com>
2023-09-27 19:35:11 +0900

    add notification_timingにenumやバリデーション追加

--- app/models/notification_timing.rb
+++ app/models/notification_timing.rb
@@ -12,4 +12,8 @@
 #  index_notification_timings_on_timing_type  (timing_type) UNIQUE
 #
 class NotificationTiming < ApplicationRecord
+  enum timing_type: { on_commented: 1, on_liked: 2, on_followed: 3 }
+
+  validates :timing_type, presence: true
+  validates :timing_type, uniqueness: true
 end
